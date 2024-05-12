4a48184ed2a6b7ef194a09e3eb5fba5743633af6
Al <alsami1209@gmail.com>
2024-05-12 11:14:47 +0600

    koo

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,19 @@
 import React from 'react'
-import Todo from './Todo';
-import Actor from './Actor';
-import Singer from './Signer';
 
 const App = () => {
 
-  const actors = ['Sakib', 'Raj', 'Ratul', 'Elius']
-  const singers = [
-    {name: "Dr. Mahafuzur Rahman", age: 68},
-    {name: 'Eva Rahman', age: 38},
-    {name: 'Shuvro Dev', age: 58},
-    {name: 'Pritom', age:28}
-  ]
-
-
+  function handleClick(){
+    alert('button click')
+  }
 
   return (
     <>
-    <div className=''>App</div>
-
-    {
-      singers.map(singer => <Singer></Singer>)
-    }
-
-
-
-
-    <Actor name={"sami"}></Actor>
-    {
-      actors.map(actor => <Actor name={actor}></Actor>)
-    }
-
-
-    {/* <Todo task="learn React" isDone={true}></Todo>
-    <Todo task="Core Concepts" isDone={false}></Todo>
-    <Todo task="Try Jsx" isDone={true}></Todo> */}
-    {/* <Person></Person> */}
-    {/* <Device name="laptop" price="55"></Device>
-    <Device name="moblie" price="20"></Device>
-    <Student grade="7" score="99"></Student>
-    <Student grade="8" score="67"></Student> */}
+      <h1>amr nam Sami ami try korsi joto tartari pari react sikar</h1>
+      <button onClick={handleClick} className='bg-orange-600 text-black px-4 py-2 rounded-lg hover:opacity-90 active:opacity-70'>click me</button>
     </>
   )
 }
 
-function Person() {
-  const age = 18;
-  const money = 188;
-  const person = {name:"Sami", age:12}
-
-  return <h3>I am {person.name} with age:{age+money}</h3>
-}
-
-function Device(props){
-  console.log(props);
-  return <h2>This Device: {props.name} price:{props.price}</h2>
-}
-
-
-function Student({grade, score}){
-  console.log(grade, score);
-  return(
-    <div>
-      <h3>This is student</h3>
-      <p>Grade: {grade}</p>
-      <p>Score: {score}</p>
-    </div>
-  )
-}
 
 
 
