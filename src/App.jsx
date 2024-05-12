3b5aaf709009a62a5d34dcf09d7ceaf1980dd5a5
Al <alsami1209@gmail.com>
2024-05-12 20:05:16 +0600

    learn something about how to count in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-
-const App = () => {
-
-  function handleClick(){
-    alert('button click')
+import {useState} from 'react'
+function Counter () {
+  const [count, setCount] = useState(10);
+  
+  function addOne(){
+    setCount(count+1); 
   }
-
+  
   return (
     <>
-      <h1>amr nam Sami ami try korsi joto tartari pari react sikar</h1>
-      <button onClick={handleClick} className='bg-orange-600 text-black px-4 py-2 rounded-lg hover:opacity-90 active:opacity-70'>click me</button>
+      <button onClick={addOne} className='text-xl bg-blue-100 m-10 p-2 rounded-md'>Click={count}</button>
     </>
   )
 }
 
-
+function App(){
+  return (
+    <div>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  );
+}
 
 
 export default App
