a299743c5b700d02008bde732e57985873335952
Al <alsami1209@gmail.com>
2024-06-07 23:12:56 +0600

    _

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
   return (
-    <div className='w-full h-screen duration-200'>
-      <div className='fixed flex flex-wrap justify-center'></div>
+    <div>
+      
     </div>
   )
 }
